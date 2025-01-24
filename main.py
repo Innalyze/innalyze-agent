c2b1c747254af16ad4a5731df0b53fd44b630caf
Adem <mohammed.adem.bourenane@gmail.com>
2025-01-24 21:39:00 +0100

    fixed static files mounting

--- main.py
+++ main.py
@@ -14,20 +14,14 @@ import os
 bootstrap()
 # from GraphAgent import get_answer
 app = FastAPI()
-# api_app = FastAPI(
-#     openapi_url=None,
-#     docs_url=None,  # Disable docs (Swagger UI)
-#     redoc_url=None,  # Disable redoc
-# )
-
-# if os.path.exists(os.path.join("front", "dist", 'index.html')):
-#     app.mount(path="/",
-#               app=StaticFiles(directory="front/dist", html=True),
-#               name="front")
+api_app = FastAPI(
+    openapi_url=None,
+    docs_url=None,  # Disable docs (Swagger UI)
+    redoc_url=None,  # Disable redoc
+)
 
 
-
-@app.websocket("/api/ask")
+@api_app.websocket("/ask")
 async def answer(socket: WebSocket):
     await socket.accept()
     question: Question = Question.model_validate_json(await socket.receive_text())
@@ -36,7 +30,9 @@ async def answer(socket: WebSocket):
     await socket.close()
 
 
-# app.mount("/api", api_app)
-# @app.post("/ask1")
-# async def answer1(question: Question ):
-#     return answer_question(question)
+app.mount("/api", api_app)
+
+if os.path.exists(os.path.join("front", "dist", 'index.html')):
+    app.mount(path="/",
+              app=StaticFiles(directory="front/dist", html=True),
+              name="front")
